bb202991c60d24b108877d724e371a16e1675d93
lukemayor15 <70761592+lukemayor15@users.noreply.github.com>
2020-11-06 16:37:32 +0000

    Minor Chanages to the AI
    Change the majoirty of the movement to run on a timer instead of  tick to improve perfromance

--- Source/RTS/Units/UnitController.cpp
+++ Source/RTS/Units/UnitController.cpp
@@ -7,8 +7,6 @@
 #include "Navigation/CrowdFollowingComponent.h"
 
 
-
-
 AUnitController::AUnitController()
 {
 	CrowdComponent = CreateDefaultSubobject<UCrowdFollowingComponent>(TEXT("CrowdFollowingComponent"));
@@ -24,62 +22,43 @@ AUnitController::AUnitController()
 	CrowdComponent->SetCrowdPathOffset(false);
 	CrowdComponent->SetCrowdSlowdownAtGoal(true);
 
-	CrowdComponent->SetCrowdSeparationWeight(200.0f);
+	CrowdComponent->SetCrowdSeparationWeight(100.0f);
 	CrowdComponent->SetCrowdCollisionQueryRange(1000.0f);
 	CrowdComponent->SetCrowdPathOptimizationRange(1000.0f);
 	CrowdComponent->SetCrowdAvoidanceQuality(ECrowdAvoidanceQuality::High);
 	CrowdComponent->SetCrowdAvoidanceRangeMultiplier(1.0f);
 
-	
 }
 void AUnitController::BeginPlay()
 {
 	Super::BeginPlay();
+	TimerTime = 0.33f;
+
+	GetWorldTimerManager().SetTimer(MemberTimerHandle, this, &AUnitController::AITimer, TimerTime, true, 0.0f);
 	MoveToStarted = false;
+	
 }
 
 //Tick Function
 void AUnitController::Tick(float DeltaTime)
 {
-	//null pointer check
 	if (PawnAsUnit)
 	{
 		//Check is pawn is Moving
 		if (PawnAsUnit->IsMoving)
 		{
-			//Have we reach the move target
-			if (PawnAsUnit->MoveTarget != PawnAsUnit->GetActorLocation())
-			{
-				//set the unit animation move
-				PawnAsUnit->MoveAnimation();
-				//Move unit to move target
-				if (MoveToStarted == false || PreMoveTarget != PawnAsUnit->MoveTarget)
-				{
-					CallMoveTo();
-				}
-				//Get direction to target
-				FVector DirectionToTarget = (PawnAsUnit->MoveTarget - PawnAsUnit->GetActorLocation());
-				//sdie wats dot produicts
-				float SidewaysDotToTarget = FVector::DotProduct(DirectionToTarget, PawnAsUnit->GetActorRightVector());
-				//The yaw we desired
-				float DeltaYawDesired = FMath::Atan2(SidewaysDotToTarget, SidewaysDotToTarget);
-				//add the desired yaw to this unit
-				PawnAsUnit->AddRotationInput(DeltaYawDesired);
-			}
-			//if we done move set is moving to false and set animation to idle
-			else
-			{
-				PawnAsUnit->IdleAnimation();
-				PawnAsUnit->IsMoving = false;
-				MoveToStarted = false;
-			}
-
-			
-			
+			//Get direction to target
+			FVector DirectionToTarget = (PawnAsUnit->MoveTarget - PawnAsUnit->GetActorLocation());
+			//sdie wats dot produicts
+			float SidewaysDotToTarget = FVector::DotProduct(DirectionToTarget, PawnAsUnit->GetActorRightVector());
+			//The yaw we desired
+			float DeltaYawDesired = FMath::Atan2(SidewaysDotToTarget, SidewaysDotToTarget);
+			//add the desired yaw to this unit
+			PawnAsUnit->AddRotationInput(DeltaYawDesired);
 		}
 	}
-	
 }
+
 //when possed set PawnAsUnit to the possed unit
 void AUnitController::OnPossess(APawn* InPawn)
 {
@@ -93,14 +72,56 @@ void AUnitController::OnUnPossess()
 	PawnAsUnit = nullptr;
 }
 
+//AI timer, this is used to update what the AI is doing
+void AUnitController::AITimer()
+{
+	//null pointer check
+	if (PawnAsUnit)
+	{
+		//Check is pawn is Moving
+		if (PawnAsUnit->IsMoving)
+		{
+			//Have we reach the move target
+			FVector ActorLoc = PawnAsUnit->GetActorLocation();
+			ActorLoc.Z = 0.0f;
+
+			//Get the diffrence between the two vectors
+			//first minus the two, then called the size functino witch returns the length of the vector
+			FVector InRangeCheck = PawnAsUnit->MoveTarget - ActorLoc;
+			int vectorLength = InRangeCheck.Size();
+			if (vectorLength > 20.0f && vectorLength > 20.0f)
+			{
+				//set the unit animation move
+				PawnAsUnit->MoveAnimation();
+				//Move unit to move target
+				if (MoveToStarted == false || PreMoveTarget != PawnAsUnit->MoveTarget)
+				{
+					CallMoveTo();
+				};
+			}
+			//If we done moving 
+			else
+			{
+				//set the unit animation to idle
+				//set the unit ISmoving to false and the controller moveToStarted to false
+				PawnAsUnit->IdleAnimation();
+				PawnAsUnit->IsMoving = false;
+				MoveToStarted = false;
+			}
+		}
+	}
+}
+
 void AUnitController::CallMoveTo()
 {
 	if (PawnAsUnit != nullptr)
 	{
+		//Move to movetarget
 		MoveToLocation(PawnAsUnit->MoveTarget, 20.0f, false, true, true, false,0,false);
+		//we have started to Move
 		MoveToStarted = true;
 	}
-
+	//set the PreMoveTarget to new MoveTarget
 	PreMoveTarget = PawnAsUnit->MoveTarget;
 
 }
--- Source/RTS/Units/UnitController.h
+++ Source/RTS/Units/UnitController.h
@@ -18,22 +18,38 @@ class RTS_API AUnitController : public AAIController
 
 	virtual void Tick(float DeltaTime) override;
 
+
 	virtual void BeginPlay() override;
 	//called when the unitcontroller is posssesd
 	virtual void OnPossess(class APawn* InPawn) override;
 	//called when the unitcontroller UnPossess a unit
 	virtual void OnUnPossess() override;
 
+	//Replacing the tick function to imporve performance
+	virtual void AITimer();
+	//how often the AITimer function is called.
+	float TimerTime;
+
+	//offset value allowed betwen unit and targeted posistion
+	float UnitOffset;
+
+	//Set the actor to move to move target
 	virtual void CallMoveTo();
 
+	//Bool to check if unit is moving
 	UPROPERTY(EditAnywhere)
 	uint32 MoveToStarted : 1;
 
+	//
 	UPROPERTY(EditAnywhere)
 	class UCrowdFollowingComponent* CrowdComponent;
 
+	//Previous target to check if the move target has been updated
 	UPROPERTY(EditAnywhere)
 	FVector PreMoveTarget;
+
+	/* Handle to manage the timer */
+	FTimerHandle MemberTimerHandle;
 
 protected:
 	//poitner to Unit class
